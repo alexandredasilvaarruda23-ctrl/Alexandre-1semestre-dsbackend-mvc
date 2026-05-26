5d9dc840c2bf286f21906ff18d02c639097e0d95
alexandredasilvaarruda23-ctrl <alexandredasilvaarruda23@gmail.com>
2026-05-25 22:21:51 -0300

    exclusão e inclusão de usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -36,10 +36,45 @@ const getUserById = (req, res) => {
 }
 
 
+   const updateUser = (req, res) => {
+      const id = Number(req.params.id)
+      const { name } = req.body
+      const newData = {
+         name : name
+   }
+
+   const updateUser = userModel.update(id, newData)
+
+  if(!updateUser) {
+      return res.status(404).json({
+         message: "Usuário não encontrado"
+      })
+   }
+
+   return res.json(updateUser)
+}
+
+  const deletedUser = (req, res) => {
+   const id = Number(req.params.id)
+
+   const deletedUser = userModel.remove(id)
+
+if(!deletedUser) {
+      return res.status(404).json({
+         message: "Usuário não encontrado"
+      })
+   }
+
+   return res.json({
+      message:"Usuário removido"
+   })
+}
 module.exports = {
    createUser,
    getALLUsers,
-   getUserById
+   getUserById,
+   updateUser,
+   deletedUser
 
 }
 
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -16,10 +16,35 @@
         return userList.find( user => user.id=== id)
     }
 
+    const update = (id, data) => {
+        const user = userList.find(user => user.id === id)
+
+        if( !user) return null
+
+        if (data.name) 
+            {user.name = data.name}
+         
+        return user
+    }
+
+    const remove = (id) => {
+        const index = userList.findIndex( user => user.id === id)
+
+        if ( index === -1) return null
+
+        const deleted = userList[index]
+
+        userList.splice(index, 1)
+
+        return deleted
+    }
+
     module.exports= {
         findALL,
         create,
-        findById
+        findById,
+        update,
+        remove
     } 
 
     
